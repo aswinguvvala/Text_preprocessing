949ddb55c734a1892b36bb5268afa1237c993636
aswinguvvala <85016907+aswinguvvala@users.noreply.github.com>
2023-04-13 13:40:19 -0500

    Create text_preprocessing.py

---
+++ text_preprocessing.py
@@ -0,0 +1,267 @@
+import pandas as pd
+import numpy as np
+import nltk
+import string
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import confusion_matrix
+from sklearn import metrics
+from sklearn.metrics import roc_curve, auc
+from nltk.stem.porter import PorterStemmer
+
+import re
+
+import string
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+from gensim.models import Word2Vec
+from gensim.models import KeyedVectors
+import pickle
+
+from tqdm import tqdm
+import os
+
+
+# Reading Data
+
+f = open("reviews.csv", "r")
+print(f.read())
+
+
+
+
+# Give reviews with Score>3 a positive rating, and reviews with a score<3 a negative rating.
+def partition(x):
+    if x < 3:
+        return 0
+    return 1
+
+#changing reviews with score less than 3 to be positive and vice-versa
+actualScore = filtered_data['Score']
+positiveNegative = actualScore.map(partition)
+filtered_data['Score'] = positiveNegative
+print("Number of data points in our data", filtered_data.shape)
+filtered_data.head(3)
+
+
+#  Exploratory Data Analysis
+
+# Data Cleaning: Deduplication
+
+
+
+#Sorting data according to ProductId in ascending order
+sorted_data=filtered_data.sort_values('ProductId', axis=0, ascending=True, inplace=False, kind='quicksort', na_position='last')
+
+
+#Deduplication of entries
+final=sorted_data.drop_duplicates(subset={"UserId","ProfileName","Time","Text"}, keep='first', inplace=False)
+final.shape
+
+
+#Checking to see how much % of data still remains
+(final['Id'].size*1.0)/(filtered_data['Id'].size*1.0)*100
+
+
+final=final[final.HelpfulnessNumerator<=final.HelpfulnessDenominator]
+
+
+#Before starting the next phase of preprocessing the number of entries left
+print(final.shape)
+
+#How many positive and negative reviews are present in our dataset?
+final['Score'].value_counts()
+
+
+# Text Preprocessing.
+
+
+# printing some random reviews
+sent_0 = final['Text'].values[0]
+print(sent_0)
+print("="*50)
+
+sent_1000 = final['Text'].values[1000]
+print(sent_1000)
+print("="*50)
+
+sent_1500 = final['Text'].values[1500]
+print(sent_1500)
+print("="*50)
+
+sent_4900 = final['Text'].values[4900]
+print(sent_4900)
+print("="*50)
+
+
+# remove urls from text python
+sent_0 = re.sub(r"http\S+", "", sent_0)
+sent_1000 = re.sub(r"http\S+", "", sent_1000)
+sent_150 = re.sub(r"http\S+", "", sent_1500)
+sent_4900 = re.sub(r"http\S+", "", sent_4900)
+
+print(sent_0)
+
+
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(sent_0, 'lxml')
+text = soup.get_text()
+print(text)
+print("="*50)
+
+soup = BeautifulSoup(sent_1000, 'lxml')
+text = soup.get_text()
+print(text)
+print("="*50)
+
+soup = BeautifulSoup(sent_1500, 'lxml')
+text = soup.get_text()
+print(text)
+print("="*50)
+
+soup = BeautifulSoup(sent_4900, 'lxml')
+text = soup.get_text()
+print(text)
+
+#%%
+
+import re
+
+def decontracted(phrase):
+    phrase = re.sub(r"won't", "will not", phrase)
+    phrase = re.sub(r"can\'t", "can not", phrase)
+
+    phrase = re.sub(r"n\'t", " not", phrase)
+    phrase = re.sub(r"\'re", " are", phrase)
+    phrase = re.sub(r"\'s", " is", phrase)
+    phrase = re.sub(r"\'d", " would", phrase)
+    phrase = re.sub(r"\'ll", " will", phrase)
+    phrase = re.sub(r"\'t", " not", phrase)
+    phrase = re.sub(r"\'ve", " have", phrase)
+    phrase = re.sub(r"\'m", " am", phrase)
+    return phrase
+
+sent_1500 = decontracted(sent_1500)
+print(sent_1500)
+print("="*50)
+
+
+#remove words with numbers 
+sent_0 = re.sub("\S*\d\S*", "", sent_0).strip()
+print(sent_0)
+
+
+#remove spacial character
+sent_1500 = re.sub('[^A-Za-z0-9]+', ' ', sent_1500)
+print(sent_1500)
+
+
+stopwords= set(['br', 'the', 'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've",\
+            "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', \
+            'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their',\
+            'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', \
+            'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', \
+            'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', \
+            'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after',\
+            'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further',\
+            'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more',\
+            'most', 'other', 'some', 'such', 'only', 'own', 'same', 'so', 'than', 'too', 'very', \
+            's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', \
+            've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn',\
+            "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn',\
+            "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", \
+            'won', "won't", 'wouldn', "wouldn't"])
+
+
+
+from tqdm import tqdm
+preprocessed_reviews = []
+# tqdm is for printing the status bar
+for sentance in tqdm(final['Text'].values):
+    sentance = re.sub(r"http\S+", "", sentance)
+    sentance = BeautifulSoup(sentance, 'lxml').get_text()
+    sentance = decontracted(sentance)
+    sentance = re.sub("\S*\d\S*", "", sentance).strip()
+    sentance = re.sub('[^A-Za-z]+', ' ', sentance)
+    
+    sentance = ' '.join(e.lower() for e in sentance.split() if e.lower() not in stopwords)
+    preprocessed_reviews.append(sentance.strip())
+
+
+preprocessed_reviews[1500]
+
+
+
+#BoW
+count_vect = CountVectorizer() #in scikit-learn
+count_vect.fit(preprocessed_reviews)
+print("some feature names ", count_vect.get_feature_names()[:10])
+print('='*50)
+
+final_counts = count_vect.transform(preprocessed_reviews)
+print("the type of count vectorizer ",type(final_counts))
+print("the shape of out text BOW vectorizer ",final_counts.get_shape())
+print("the number of unique words ", final_counts.get_shape()[1])
+
+
+
+#gram
+
+
+
+count_vect = CountVectorizer(ngram_range=(1,2), min_df=10, max_features=5000)
+final_bigram_counts = count_vect.fit_transform(preprocessed_reviews)
+print("the type of count vectorizer ",type(final_bigram_counts))
+print("the shape of out text BOW vectorizer ",final_bigram_counts.get_shape())
+print("the number of unique words including both unigrams and bigrams ", final_bigram_counts.get_shape()[1])
+
+
+
+
+
+tf_idf_vect = TfidfVectorizer(ngram_range=(1,2), min_df=10)
+tf_idf_vect.fit(preprocessed_reviews)
+print("some sample features",tf_idf_vect.get_feature_names()[0:10])
+print('='*50)
+
+final_tf_idf = tf_idf_vect.transform(preprocessed_reviews)
+print("the type of count vectorizer ",type(final_tf_idf))
+print("the shape of out text TFIDF vectorizer ",final_tf_idf.get_shape())
+print("the number of unique words including both unigrams and bigrams ", final_tf_idf.get_shape()[1])
+
+
+
+
+
+
+
+# Word2Vec
+
+
+
+
+
+is_your_ram_gt_16g=False
+want_to_use_google_w2v = False
+want_to_train_w2v = True
+
+if want_to_train_w2v:
+    # min_count = 5 considers only words that occured atleast 5 times
+    w2v_model=Word2Vec(list_of_sentance,min_count=5,size=50, workers=4)
+    print(w2v_model.wv.most_similar('great'))
+    print('='*50)
+    print(w2v_model.wv.most_similar('worst'))
+    
+elif want_to_use_google_w2v and is_your_ram_gt_16g:
+    if os.path.isfile('GoogleNews-vectors-negative300.bin'):
+        w2v_model=KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
+        print(w2v_model.wv.most_similar('great'))
+        print(w2v_model.wv.most_similar('worst'))
+    else:
+        print("you don't have gogole's word2vec file, keep want_to_train_w2v = True, to train your own w2v ")
